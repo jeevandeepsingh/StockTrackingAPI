0a6d75a0955aeea8c5b4d6f9c98a68e2e0a0fde3
jeevandeepsingh <58933157+jeevandeepsingh@users.noreply.github.com>
2021-07-05 14:41:38 +0530

    Update trades.js

--- controllers/trades.js
+++ controllers/trades.js
@@ -2,17 +2,23 @@ const Trade = require('../modals/trade');
 const User = require('../modals/user');
 const tradesUpdater = require('../utils/tradesUpdater');
 
+/*
+add function will add the trade into trade database as well as in user trades array.
+Firstly tis function will verify that this operation is possible or not & then add it.
+*/
 module.exports.add = async (req, res) => {
     try {
         const {token} = req.params;
         const {ticker, shareprice, type, quantity} = req.body;
         
-        const user = await User.findOne({token});
+        const user = await User.findOne({token});//Find user using token ID
         const tid = user.tidcount+1; 
 
-        if(type === "BUY")
+        if(type === "BUY")//If trade type = BUY
         {
-            if(user.securities.has(ticker))
+            
+            //If we already have some trades in our database of same security(bank).
+            if(user.securities.has(ticker)) 
             {
                 const data = user.securities.get(ticker);
 
@@ -31,7 +37,7 @@ module.exports.add = async (req, res) => {
                 await user.save();
     
             }
-            else
+            else //If BUYING this security(bank) trade first time.
             {
                 let averagebuyprice = shareprice, noofshares = quantity;
                 let prevaveragebuyprice = 0, prevquantity = 0;
@@ -46,16 +52,16 @@ module.exports.add = async (req, res) => {
 
             }
         }
-        else
+        else//If trade type = SELL
         {
-            if(user.securities.has(ticker))
+            if(user.securities.has(ticker))//If we already have some trades in our database of same security(bank).
             {
                 const data = user.securities.get(ticker);
 
                 let averagebuyprice = data[0];
                 let prevaveragebuyprice = data[0], prevquantity = data[1];
 
-                if(quantity > prevquantity)
+                if(quantity > prevquantity)//current available shares should be more than selling shares.
                 {
                     return res.status(400).send({'error': 'Cannot sell shares more than current available shares'});
                 }
@@ -71,7 +77,7 @@ module.exports.add = async (req, res) => {
                 await user.save();
     
             }
-            else
+            else//If SELLING this security(bank) trade first time.
             {
                 return res.status(400).send({'error': 'User does not have any shares of this company'});
             }
@@ -83,6 +89,7 @@ module.exports.add = async (req, res) => {
     }
 }
 
+//Details function simply returns the details of user 
 module.exports.details = async (req, res) => {
     
     try {
@@ -125,7 +132,10 @@ module.exports.details = async (req, res) => {
     }
 }
 
-
+/*
+update function will update the trade into trade database as well as in user trades array.
+Firstly tis function will verify that this operation is possible or not & then update it.
+*/
 module.exports.update = async (req, res) => {
     try {
 
@@ -141,8 +151,9 @@ module.exports.update = async (req, res) => {
         let oldticker = user.trades[tradeIndex].ticker;
 
         const trades = await Trade.find({token: token, ticker: oldticker});
-        let idx = trades.findIndex(trade => trade.tid === tid);
+        let idx = trades.findIndex(trade => trade.tid === tid);//Find index of the trade which we have to delete
         
+        //updating the old trade details
         trades[idx].shareprice = shareprice;
         trades[idx].type = type;
         trades[idx].quantity = quantity;
@@ -151,19 +162,23 @@ module.exports.update = async (req, res) => {
         trades[idx].prevquantity = 0;    
         trades[idx].prevaveragebuyprice = 0;
 
+         //If we are updating a trade values but the security(bank) is same
         if(ticker === oldticker)
         {
             await tradesUpdater.updateTrades(req, res, trades, ticker, user);//Bcz we have to update trade for same company.
             return res.status(200).send({'success': 'Successfully added Trade'});        
         }
-        else
+        else//If we are updating a trade values of different security(bank).
         {
-            const response = await tradesUpdater.updateTrades(req, res, trades, oldticker, user);//Bcz we have to update trade for Diff company we don't consider cur trade.                
+            //Firstly we check after updating the old trade will not effect the other trades of old security(bank).
+            const response = await tradesUpdater.updateTrades(req, res, trades, oldticker, user);                
             
-            if(response.statusCode != 400)
+            if(response.statusCode != 400)//Updation is possible then
             {
                 const tradeB = await Trade.find({token: token, ticker: ticker});
-                await tradesUpdater.updateTrades(req, res, tradeB, ticker, user);//Bcz we have to update trade for Diff company we don't consider cur trade.
+                
+                //we check updating the this trade will not effect the other trades of new security(bank) & then Update the trade in for new security(bank).
+                await tradesUpdater.updateTrades(req, res, tradeB, ticker, user);
             
                 return res.status(200).send({'success': 'Successfully added Trade'});        
             }
@@ -174,7 +189,10 @@ module.exports.update = async (req, res) => {
     }
 }   
 
-
+/*
+delete function will delete the trade into trade database as well as in user trades array.
+Firstly tis function will verify that this operation is possible or not & then delete it.
+*/
 module.exports.delete = async (req, res) => {
     try {
         const {token} = req.params;
@@ -185,22 +203,23 @@ module.exports.delete = async (req, res) => {
             path: 'trades'
         });
 
-        let tradeIndex = user.trades.findIndex(trade => trade.tid === tid);
+        let tradeIndex = user.trades.findIndex(trade => trade.tid === tid);//Find index of the trade which we have to delete
         let ID = user.trades[tradeIndex]._id;
         let ticker = user.trades[tradeIndex].ticker;
 
-        const trades = await Trade.find({token: token, ticker: ticker});
+        const trades = await Trade.find({token: token, ticker: ticker});//extract all the trades from TRADE collection of same bank for particular user. 
 
+        //updating the old trade details
         trades[tradeIndex].shareprice = 0;
         trades[tradeIndex].quantity = 0;
         trades[tradeIndex].averagebuyprice = 0;
         trades[tradeIndex].prevquantity = 0;    
         trades[tradeIndex].prevaveragebuyprice = 0;
 
-        
+        //Firstly we check after delete the trade will not effect the other trades of security(bank).
         const response = await tradesUpdater.updateTrades(req, res, trades, ticker, user);//Bcz we have to update trade for same company.
         
-        if(response.statusCode != 400)
+        if(response.statusCode != 400)//deleteion is possible then
         {
             await Trade.findByIdAndDelete(ID);
             user.trades.splice(tradeIndex,1);
